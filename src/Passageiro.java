78b9afda6359d0a0bb2c049c098d0c0407be331a
Joaorezendelopes <142945988+Joaorezendelopes@users.noreply.github.com>
2023-12-09 20:43:17 -0300

    add src passageiro

---
+++ src/Passageiro.java
@@ -0,0 +1,50 @@
+
+public class Passageiro {
+	private String nome, email;
+	private int idade, cpf;
+
+	public Passageiro() {
+
+	}
+
+	public Passageiro(String nome, String email, int idade, int cpf) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.idade = idade;
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+}
